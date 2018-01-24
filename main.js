cf3fc68e583b663ec245792603d0bf2abdbbb22f
Petr Čermák <30902390+cermak-petr@users.noreply.github.com>
2018-01-24 17:12:42 +0100

    Update main.js

--- main.js
+++ main.js
@@ -2,13 +2,11 @@ const Apify = require('apify');
 const BigQuery = require('@google-cloud/bigquery');
 
 Apify.main(async () => {
-    
-    // Imports the Google Cloud client library
-const BigQuery = require('@google-cloud/bigquery');
 
 const projectId = "test-project-154214";
 const datasetId = "my_project_154214";
 const tableId = "test-project-154214:my_project_154214.my_new_table";
+    
 const rows = [{
     'hashtag': 'test_hashtag',
     'profile_url': 'test_url',
@@ -27,25 +25,25 @@ const rows = [{
 
 // Creates a client
 const bigquery = new BigQuery({
-  projectId: projectId,
+    projectId: projectId,
 });
 
 // Inserts data into a table
 bigquery
-  .dataset(datasetId)
-  .table(tableId)
-  .insert(rows)
-  .then(() => {
-    console.log(`Inserted ${rows.length} rows`);
-  })
-  .catch(err => {
+    .dataset(datasetId)
+    .table(tableId)
+    .insert(rows)
+    .then(() => {
+        console.log(`Inserted ${rows.length} rows`);
+    })
+    .catch(err => {
     if (err && err.name === 'PartialFailureError') {
-      if (err.errors && err.errors.length > 0) {
-        console.log('Insert errors:');
-        err.errors.forEach(err => console.error(err));
-      }
+        if (err.errors && err.errors.length > 0) {
+            console.log('Insert errors:');
+            err.errors.forEach(err => console.error(err));
+        }
     } else {
-      console.error('ERROR:', err);
+        console.error('ERROR:', err);
     }
   });
     
